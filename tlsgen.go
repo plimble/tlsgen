d5fcd970b342495a3e3f6691a49f108ec0595d16
Witoo Harianto <witoo@outlook.com>
2015-05-14 15:38:27 +0700

    init

---
+++ tlsgen.go
@@ -0,0 +1,334 @@
+package main
+
+// import (
+// 	"flag"
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"os/exec"
+// 	"runtime/debug"
+// )
+
+// var (
+// 	flagCertificateAuthorityName = flag.String("cn", "", "")
+// 	flagHostName                 = flag.String("h", "", "")
+// )
+
+// var usage = `Usage: EasyCert [options...]
+// Options:
+//   -cn Certificate Authority Name (can be any name, but should reflect your company name.)
+//   -h  Hostname of TLS server to install the private cert/key
+// `
+
+// func main() {
+// 	flag.Usage = func() {
+// 		fmt.Fprintf(os.Stderr, usage)
+// 	}
+
+// 	flag.Parse()
+
+// 	certName := *flagCertificateAuthorityName
+// 	hostName := *flagHostName
+
+// 	if certName == "" || hostName == "" {
+// 		usageAndExit("You must supply both a -cn (certificate name) and -h (host name) parameter")
+// 	}
+
+// 	createPrivateCA(certName)
+// 	createServerCertKey(hostName)
+
+// 	log.Println("*** Operation Completed Succesfully ***")
+// 	log.Println("Private root certificate created: ", "ca.pem")
+// 	log.Println("Web server certificate created: ", "cert.pem")
+// 	log.Println("Web server key created: ", "key.pem")
+// }
+
+// func createPrivateCA(certificateAuthorityName string) {
+// 	_, err := callCommand("openssl", "genrsa", "-out", "myCA.key", "2048")
+// 	if err != nil {
+// 		log.Fatal("Could not create private Certificate Authority key")
+// 	}
+
+// 	_, err = callCommand("openssl", "req", "-x509", "-new", "-key", "myCA.key", "-out", "ca.pem", "-days", "730", "-subj", "/CN=\""+certificateAuthorityName+"\"")
+// 	if err != nil {
+// 		log.Fatal("Could not create private Certificate Authority certificate")
+// 	}
+// }
+
+// func createServerCertKey(host string) {
+// 	_, err := callCommand("openssl", "genrsa", "-out", "key.pem", "2048")
+// 	if err != nil {
+// 		log.Fatal("Could not create private server key")
+// 	}
+
+// 	_, err = callCommand("openssl", "req", "-new", "-out", "cert.pem", "-key", "key.pem", "-subj", "/CN="+host)
+// 	if err != nil {
+// 		log.Fatal("Could not create private server certificate signing request")
+// 	}
+
+// 	_, err = callCommand("openssl", "x509", "-req", "-in", "cert.pem", "-out", "cert.pem", "-CAkey", "myCA.key", "-CA", "ca.pem", "-days", "365", "-CAcreateserial", "-CAserial", "serial")
+// 	if err != nil {
+// 		log.Fatal("Could not create private server certificate")
+// 	}
+
+// }
+
+// func callCommand(command string, arg ...string) (string, error) {
+// 	out, err := exec.Command(command, arg...).Output()
+
+// 	if err != nil {
+// 		log.Println("callCommand failed!")
+// 		log.Println("")
+// 		log.Println(string(debug.Stack()))
+// 		return "", err
+// 	}
+// 	return string(out), nil
+// }
+
+// func usageAndExit(message string) {
+// 	if message != "" {
+// 		fmt.Fprintf(os.Stderr, message)
+// 		fmt.Fprintf(os.Stderr, "\n\n")
+// 	}
+// 	flag.Usage()
+// 	fmt.Fprintf(os.Stderr, "\n")
+// 	os.Exit(1)
+// }
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"github.com/Sirupsen/logrus"
+	"github.com/codegangsta/cli"
+	"io/ioutil"
+	"math/big"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "tlsgen"
+	app.Usage = "Generates web server TLS certificates"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "org",
+			Value: "",
+			Usage: "Organization name",
+		},
+		cli.StringSliceFlag{
+			Name:  "host",
+			Value: &cli.StringSlice{},
+			Usage: "Server Host",
+		},
+	}
+	app.Action = func(c *cli.Context) {
+		org := c.String("org")
+		if org == "" {
+			logrus.Fatal("org shoule not be empty")
+		}
+		host := c.StringSlice("host")
+		if len(host) == 0 {
+			logrus.Fatal("host shoule not be empty")
+		}
+		GenerateCACertificate("ca.pem", "ca-key.pem", org, 2048)
+		GenerateCert(host, "cert.pem", "key.pem", "ca.pem", "ca-key.pem", org, 2048)
+	}
+
+	app.Run(os.Args)
+}
+
+func getTLSConfig(caCert, cert, key []byte, allowInsecure bool) (*tls.Config, error) {
+	// TLS config
+	var tlsConfig tls.Config
+	tlsConfig.InsecureSkipVerify = allowInsecure
+	certPool := x509.NewCertPool()
+
+	certPool.AppendCertsFromPEM(caCert)
+	tlsConfig.RootCAs = certPool
+	keypair, err := tls.X509KeyPair(cert, key)
+	if err != nil {
+		return &tlsConfig, err
+	}
+	tlsConfig.Certificates = []tls.Certificate{keypair}
+	if allowInsecure {
+		tlsConfig.InsecureSkipVerify = true
+	}
+
+	return &tlsConfig, nil
+}
+
+func newCertificate(org string) (*x509.Certificate, error) {
+	now := time.Now()
+	// need to set notBefore slightly in the past to account for time
+	// skew in the VMs otherwise the certs sometimes are not yet valid
+	notBefore := time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute()-5, 0, 0, time.Local)
+	notAfter := notBefore.Add(time.Hour * 24 * 1080)
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return nil, err
+	}
+
+	return &x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{org},
+		},
+		NotBefore: notBefore,
+		NotAfter:  notAfter,
+
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		BasicConstraintsValid: true,
+	}, nil
+
+}
+
+// GenerateCACertificate generates a new certificate authority from the specified org
+// and bit size and stores the resulting certificate and key file
+// in the arguments.
+func GenerateCACertificate(certFile, keyFile, org string, bits int) error {
+	template, err := newCertificate(org)
+	if err != nil {
+		return err
+	}
+
+	template.IsCA = true
+	template.KeyUsage |= x509.KeyUsageCertSign
+
+	priv, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		return err
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, template, template, &priv.PublicKey, priv)
+	if err != nil {
+		return err
+	}
+
+	certOut, err := os.Create(certFile)
+	if err != nil {
+		return err
+	}
+
+	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	certOut.Close()
+
+	keyOut, err := os.OpenFile(keyFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+
+	}
+
+	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
+	keyOut.Close()
+
+	return nil
+}
+
+// GenerateCert generates a new certificate signed using the provided
+// certificate authority files and stores the result in the certificate
+// file and key provided.  The provided host names are set to the
+// appropriate certificate fields.
+func GenerateCert(hosts []string, certFile, keyFile, caFile, caKeyFile, org string, bits int) error {
+	template, err := newCertificate(org)
+	if err != nil {
+		return err
+	}
+	// client
+	if len(hosts) == 1 && hosts[0] == "" {
+		template.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth}
+		template.KeyUsage = x509.KeyUsageDigitalSignature
+	} else { // server
+		template.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth}
+		for _, h := range hosts {
+			if ip := net.ParseIP(h); ip != nil {
+				template.IPAddresses = append(template.IPAddresses, ip)
+
+			} else {
+				template.DNSNames = append(template.DNSNames, h)
+			}
+		}
+	}
+
+	tlsCert, err := tls.LoadX509KeyPair(caFile, caKeyFile)
+	if err != nil {
+		return err
+
+	}
+
+	priv, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		return err
+
+	}
+
+	x509Cert, err := x509.ParseCertificate(tlsCert.Certificate[0])
+	if err != nil {
+		return err
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, template, x509Cert, &priv.PublicKey, tlsCert.PrivateKey)
+	if err != nil {
+		return err
+	}
+
+	certOut, err := os.Create(certFile)
+	if err != nil {
+		return err
+
+	}
+
+	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	certOut.Close()
+
+	keyOut, err := os.OpenFile(keyFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+
+	}
+
+	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
+	keyOut.Close()
+
+	return nil
+}
+
+func ValidateCertificate(addr, caCertPath, serverCertPath, serverKeyPath string) (bool, error) {
+	caCert, err := ioutil.ReadFile(caCertPath)
+	if err != nil {
+		return false, err
+	}
+
+	serverCert, err := ioutil.ReadFile(serverCertPath)
+	if err != nil {
+		return false, err
+	}
+
+	serverKey, err := ioutil.ReadFile(serverKeyPath)
+	if err != nil {
+		return false, err
+	}
+
+	tlsConfig, err := getTLSConfig(caCert, serverCert, serverKey, false)
+	if err != nil {
+		return false, err
+	}
+
+	dialer := &net.Dialer{
+		Timeout: time.Second * 2,
+	}
+
+	_, err = tls.DialWithDialer(dialer, "tcp", addr, tlsConfig)
+	if err != nil {
+		return false, nil
+	}
+
+	return true, nil
+}
